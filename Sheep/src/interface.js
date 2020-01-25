483aaa2795b386931f5e99bbca21c5c057c6ba87
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-01-25 23:46:10 +0000

    Creates html, css and interface. Click on each button to make a different sheep disappear

---
+++ Sheep/src/interface.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+
+
+    $("#herdsheep1").click(function(){
+      $('#sheep1').hide();
+    });
+
+    $("#herdsheep2").click(function(){
+      $('#sheep2').hide();
+    });
+
+    $("#herdsheep3").click(function(){
+      $('#sheep3').hide();
+    });
+
+    $("#herdsheep4").click(function(){
+      $('#sheep4').hide();
+    });
+
+    $("#herdsheep5").click(function(){
+      $('#sheep5').hide();
+    });
+
+    $("#herdsheep6").click(function(){
+      $('#sheep6').hide();
+    });
+
+    $("#makesheepappear").click(function(){
+      $().show();
+    });
+
+   
+
+    
+});
